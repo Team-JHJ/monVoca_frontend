0433c38ee9b876101c7bd3ffe420519b500921ee
khanna01 <govmfkdtm@gmail.com>
2024-11-11 20:42:51 +0900

    베이스

---
+++ src/pages/Base-page.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header.jsx'
+import { Outlet } from 'react-router-dom'
+
+export default function BasePage() {
+    return (
+        <div className="flex min-h-screen justify-center bg-gray-200">
+            <div className="relative flex h-screen w-full max-w-md flex-col">
+                <Header />
+                <div className="h-full overflow-hidden bg-[#EFF6FF] p-3">
+                    <div className="h-full w-full overflow-y-auto bg-amber-200">
+                        <Outlet />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
