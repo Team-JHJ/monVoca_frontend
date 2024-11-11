4ebd3dc13aaa6965f8c6414000544b5aeb47b04c
khanna01 <govmfkdtm@gmail.com>
2024-11-11 20:42:23 +0900

    라우터

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export default function App() {
-    return <h1 className="text-3xl font-bold text-blue-300">Hello monVoca!</h1>
+import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import BasePage from '@/pages/base-page.jsx'
+import MainPage from '@/pages/main-page.jsx'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainPage />,
+    },
+])
+
+function App() {
+    return <RouterProvider router={router} />
 }
+
+export default App
